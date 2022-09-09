1cebc4de7c5dcb9fe466c0d8635d976011e39a6a
Guiwoo <park.guiwoo@hotmail.com>
2022-09-09 23:38:36 +0900

    회원 가입시 필요한 레포지토리 추가 [스포츠] [멤버 이메일테이블] [멤버닉네임,휴대전화]

--- src/main/java/com/workduo/member/existmember/repository/ExistMemberRepository.java
+++ src/main/java/com/workduo/member/existmember/repository/ExistMemberRepository.java
@@ -1,2 +1,8 @@
-package com.workduo.member.existmember.repository;public class ExistMemberRepository {
+package com.workduo.member.existmember.repository;
+
+import com.workduo.member.existmember.entity.ExistMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExistMemberRepository extends JpaRepository<ExistMember,String> {
+    boolean existsByMemberEmail(String email);
 }
