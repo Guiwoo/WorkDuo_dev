686feb25e21346936fcffd4ef313e78568bb90f8
Guiwoo <park.guiwoo@hotmail.com>
2022-09-09 23:37:17 +0900

    회원가입 에러 코드 추가

--- src/main/java/com/workduo/error/member/type/MemberErrorCode.java
+++ src/main/java/com/workduo/error/member/type/MemberErrorCode.java
@@ -7,14 +7,24 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum MemberErrorCode {
-
-    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"❌ 서버 내부 에러 입니다."),
+    //일반 에러
     MEMBER_EMAIL_ERROR(HttpStatus.FORBIDDEN,"❌ 이메일 계정 이 존재하지 않습니다."),
+    MEMBER_EMAIL_FORM(HttpStatus.FORBIDDEN,"❌ 이메일 형태가 아닙니다."),
     MEMBER_PASSWORD_ERROR(HttpStatus.FORBIDDEN,"❌ 패스워드 가 일치하지 않습니다."),
+    MEMBER_PASSWORD_POLICY(HttpStatus.FORBIDDEN,"❌ 최소 8 최대20, 최소 문자 1개,숫자1개,특수문자 1개를 포함하고 있어야 합니다."),
+    // 중복체크 에러
+    MEMBER_EMAIL_DUPLICATE(HttpStatus.FORBIDDEN,"❌ 이미 존재하는 이메일 입니다."),
+    MEMBER_NICKNAME_DUPLICATE(HttpStatus.FORBIDDEN,"❌ 이미 존재하는 닉네임 입니다."),
+    MEMBER_PHONE_DUPLICATE(HttpStatus.FORBIDDEN,"❌ 이미 존재하는 전화번호 입니다."),
+    // 삭제예정
+    MEMBER_SIGG_ERROR(HttpStatus.FORBIDDEN,"❌ 삭제예정."),
+    MEMBER_SPORT_ERROR(HttpStatus.FORBIDDEN,"❌ 삭제예정."),
+    // 회원 상태 에러
     MEMBER_STOP_ERROR(HttpStatus.FORBIDDEN,"❌ 정지된 회원 입니다."),
     MEMBER_WITHDRAW_ERROR(HttpStatus.FORBIDDEN,"❌ 탈퇴한 회원 입니다."),
-
     MEMBER_REFRESH_TOKEN_ERROR(HttpStatus.FORBIDDEN,"❌ 탈퇴한 회원 입니다.");
+
+
 
     private final HttpStatus httpStatus;
     private final String message;
