0d5d007b3d89fde8355fb93822b9dc9fabfc51e5
Guiwoo <park.guiwoo@hotmail.com>
2022-09-11 19:23:57 +0900

    Logout 캐시 쿠키 데이터 컨텍스 삭제 코드 추가

--- src/main/java/com/workduo/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/workduo/configuration/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import com.workduo.configuration.security.handler.LogoutSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -18,6 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
+
+import static org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.*;
+import static org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.EXECUTION_CONTEXTS;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +31,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
     private final JwtAuthenticationFilter authenticationFilter;
+    private static final ClearSiteDataHeaderWriter.Directive[] SOURCE =
+            {CACHE, COOKIES, STORAGE, EXECUTION_CONTEXTS};
 
     @Bean
     public LogoutSuccessHandler getLogoutHandler(){
@@ -59,21 +67,27 @@ public class SecurityConfiguration {
         //접근 누구나 가능
         http.authorizeRequests()
                 .antMatchers(
-                        "/h2-console/**","/api/v1/member/login","/api/v1/member/logout",
-                        "/api/v1/member"
-                ).permitAll();
+                        "/h2-console/**",
+                        "/api/v1/member/login",
+                        "/api/v1/member/logout"
+                ).permitAll()
+                .antMatchers(HttpMethod.POST,"/api/v1/member")
+                .permitAll();
 
         http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 
         // 로그인 된 유저(토큰 이 있는),권한 이 있는 접근
         http.authorizeRequests()
-                .antMatchers("/api/v1/auth")
+                .antMatchers(HttpMethod.PATCH,"/api/v1/member")
+                .hasAnyAuthority( "ROLE_MEMBER", "ROLE_ADMIN")
+                .antMatchers("/api/v1/member")
                 .hasAnyAuthority( "ROLE_MEMBER", "ROLE_ADMIN");
 
         http.logout()
                 .logoutUrl("/api/v1/member/logout")
                 .permitAll()
+                .addLogoutHandler(new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(SOURCE)))
                 .logoutSuccessHandler(getLogoutHandler());
 
         return http.build();
--- src/main/java/com/workduo/configuration/security/handler/LogoutSuccessHandler.java
+++ src/main/java/com/workduo/configuration/security/handler/LogoutSuccessHandler.java
@@ -3,13 +3,17 @@ package com.workduo.configuration.security.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.*;
 
 public class LogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
     private int statusCode;
@@ -25,9 +29,11 @@ public class LogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
         response.setStatus(this.statusCode);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
+
         Map<String,Object> map = new HashMap<>();
         map.put("success","T");
         ObjectMapper o = new ObjectMapper();
+
         response.getWriter().println(o.writeValueAsString(map));
     }
 
