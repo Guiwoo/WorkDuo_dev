e39829a809ff3bb16cf45b4236adcc6ce38b5fdf
Guiwoo <park.guiwoo@hotmail.com>
2022-09-11 19:24:41 +0900

    멤버 가입에 따른 저장에 필요한 레포지토리 생성

--- src/main/java/com/workduo/member/area/repository/MemberActiveAreaRepository.java
+++ src/main/java/com/workduo/member/area/repository/MemberActiveAreaRepository.java
@@ -1,7 +1,13 @@
 package com.workduo.member.area.repository;
 
+import com.workduo.area.siggarea.entity.SiggArea;
 import com.workduo.member.area.entity.MemberActiveArea;
+import com.workduo.member.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberActiveAreaRepository extends JpaRepository<MemberActiveArea,Long> {
+    List<MemberActiveArea> findAllByMember(Member m);
 }
