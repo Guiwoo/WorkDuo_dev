73c0dc8b2415e75a0d132046815c9f388ce75582
Guiwoo <park.guiwoo@hotmail.com>
2022-09-21 21:24:57 +0900

    컨탠트 리스트 조회를 위한 필요 디티오 작성
     feature

---
+++ src/main/java/com/workduo/member/content/dto/MemberContentListDto.java
@@ -0,0 +1,67 @@
+package com.workduo.member.content.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Builder
+public class MemberContentListDto {
+    private Long id;
+    private String title;
+    private String content;
+    private boolean noticeYn;
+    private int sortValue;
+    private Long memberId;
+    private String username;
+    private String profileImg;
+    private boolean deletedYn;
+    private LocalDateTime createdAt;
+    private Long count;
+    private List<MemberContentImageDto> memberContentImages;
+
+    public static MemberContentListDto from(
+            MemberContentDto memberContentDto,
+            List<MemberContentImageDto> memberContentImageDto
+    ){
+        return MemberContentListDto.builder()
+                .id(memberContentDto.getId())
+                .title(memberContentDto.getTitle())
+                .content(memberContentDto.getContent())
+                .noticeYn(memberContentDto.isNoticeYn())
+                .sortValue(memberContentDto.getSortValue())
+                .memberId(memberContentDto.getMemberId())
+                .username(memberContentDto.getUsername())
+                .profileImg(memberContentDto.getProfileImg())
+                .deletedYn(memberContentDto.isDeletedYn())
+                .createdAt(memberContentDto.getCreatedAt())
+                .count(memberContentDto.getCount())
+                .memberContentImages(memberContentImageDto)
+                .build();
+
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response{
+        private String success;
+        private Page<MemberContentListDto> result;
+
+        public static Response from(
+                Page<MemberContentListDto> lists
+        ){
+            return Response.builder()
+                    .success("T")
+                    .result(lists)
+                    .build();
+        }
+    }
+}
