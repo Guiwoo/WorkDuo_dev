ec532ced3f45644a4311a2a2c42236ae7ef23a5a
Guiwoo <park.guiwoo@hotmail.com>
2022-09-23 19:02:51 +0900

    서비스 작성
     -feature

--- src/main/java/com/workduo/member/content/repository/MemberContentRepository.java
+++ src/main/java/com/workduo/member/content/repository/MemberContentRepository.java
@@ -1,10 +1,11 @@
 package com.workduo.member.content.repository;
 
 import com.workduo.member.content.entity.MemberContent;
+import com.workduo.member.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberContentRepository extends JpaRepository<MemberContent,Long> {
-
+    boolean existsByMemberAndId(Member m,Long contentId);
 }
