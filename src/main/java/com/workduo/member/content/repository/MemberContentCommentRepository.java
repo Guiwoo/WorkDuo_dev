f61908940e9fdf19046261bb53fd6cb5542670a4
Guiwoo <park.guiwoo@hotmail.com>
2022-09-28 22:09:07 +0900

    Repository 작성
     -feature

--- src/main/java/com/workduo/member/content/repository/MemberContentCommentRepository.java
+++ src/main/java/com/workduo/member/content/repository/MemberContentCommentRepository.java
@@ -2,16 +2,25 @@ package com.workduo.member.content.repository;
 
 import com.workduo.member.content.entity.MemberContent;
 import com.workduo.member.content.entity.MemberContentComment;
+import com.workduo.member.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberContentCommentRepository extends JpaRepository<MemberContentComment,Long> {
+
+    Optional<MemberContentComment> findByIdAndMemberAndMemberContentAndDeletedYn(
+            Long mcc,Member m, MemberContent mc,boolean deleted);
     List<MemberContentComment> findAllByMemberContent(MemberContent memberContent);
     @Modifying
     @Query("delete from MemberContentComment mcc where mcc.memberContent = :memberContent")
     void deleteAllByMemberContent(@Param("memberContent") MemberContent memberContent);
+
+    @Modifying
+    @Query("delete from MemberContentComment mcc where mcc.id = :commentId")
+    void deleteById(@Param("commentId") Long commentId);
 }
