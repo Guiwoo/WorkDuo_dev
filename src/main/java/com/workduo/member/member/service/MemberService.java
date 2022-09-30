5dcbbb08f04aae69e214d618e1753e83765a1193
Guiwoo <park.guiwoo@hotmail.com>
2022-09-30 22:23:19 +0900

    edit 수정에 따른 service 코드 수정
     -feature

--- src/main/java/com/workduo/member/member/service/MemberService.java
+++ src/main/java/com/workduo/member/member/service/MemberService.java
@@ -8,11 +8,14 @@ import com.workduo.member.member.dto.MemberEdit;
 import com.workduo.member.member.dto.MemberLogin;
 import com.workduo.member.member.dto.auth.MemberAuthenticateDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface MemberService extends UserDetailsService {
     MemberAuthenticateDto authenticateUser(MemberLogin.Request member);
     void createUser(MemberCreate.Request create);
-    void editUser(MemberEdit.Request req);
+    void editUser(MemberEdit.Request req, MultipartFile multipartFile);
 
     void changePassword(MemberChangePassword.Request req);
 
